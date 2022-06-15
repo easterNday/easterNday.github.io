bacad380b3d6fffcdb5d10800194061e640e6eb3
easterNday <849919718@qq.com>
2022-06-16 03:28:25 +0800

    chore: 完成VuePress环境的初步配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+    }
+}
